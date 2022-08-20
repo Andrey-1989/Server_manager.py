398be53f0ddf5a648851d4093113940ce6ef16f0
Andrey-1989 <96699032+Andrey-1989@users.noreply.github.com>
2022-08-20 16:27:43 +0300

    Update Server_manager.py

--- Server_manager.py
+++ Server_manager.py
@@ -5,13 +5,4 @@ from config import vk_api_token
 
 
 server1 = Server(vk_api_token, 172998024, "server1")
-# vk_api_token - API токен, который мы ранее создали
-# 172998024 - id сообщества-бота
-# "server1" - имя сервера
-
-server1.test()
-
-def start(self):
-        for event in self.long_poll.listen():
-            print(event)
 server1.start()
